02cc26174eb737490efdcca9bb1694f7d7140743
오민섭 <you@example.com>
2020-01-08 00:07:21 +0900

    pm2 적용

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+  apps: [
+    {
+      name: 'lafu-api',
+      script: './src/server.ts',
+      instances: "4",
+      ignore_watch: ["node_modules"],
+      exec_mode: "cluster",
+      interpreter: "ts-node",
+      wait_ready: true,
+      listen_timeout: 50000,
+      kill_timeout: 5000,    
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+};
