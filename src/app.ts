7a89a78555cedaab675c2c0f8f44941957c222f1
오민섭 <you@example.com>
2020-01-29 00:38:35 +0900

    lafu server cors setting

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,13 @@ import helmet from 'helmet';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import compresion from 'compression';
+import cors, { CorsOptions } from 'cors';
 import createLoaders, { Loaders } from './lib/createLoader';
 import schema from './graphql/schema';
 import routes from './routes';
+import { consumeUser } from './lib/tokens';
 
+const allowedHosts = [/^https:\/\/lafu.io$/];
 const app = express();
 
 app.use(
@@ -20,6 +23,28 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === 'development') {
   app.use(logger('dev'));
 }
+app.use((req, res, next) => {
+  const valid = allowedHosts.some(regex => regex.test(req.header('Origin')!));
+  if (!valid) return next();
+  const corsOptions: CorsOptions = Object.assign(
+    {
+      origin: true,
+      credentials: true
+    },
+    req.method === 'OPTIONS'
+      ? {
+          allowedHeaders: [
+            'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Cookie'
+          ],
+          methods: ['GET,HEAD,PUT,POST,DELETE,PATCH']
+        }
+      : {}
+  );
+
+  cors(corsOptions);
+  return next();
+});
+app.use(consumeUser);
 app.use(routes);
 
 export type ApolloContext = {
@@ -33,7 +58,12 @@ const apollo = new ApolloServer({
     try {
       return {
         user_id: res.locals.user_id,
-        loaders: createLoaders()
+        loaders: createLoaders(),
+        ip: req.ip,
+        unsetCookie: () => {
+          req.cookies('access_token');
+          req.cookies('referesh_token');
+        }
       };
     } catch (e) {
       return {};
@@ -41,6 +71,6 @@ const apollo = new ApolloServer({
   },
   tracing: process.env.NODE_ENV === 'development'
 });
-apollo.applyMiddleware({ app });
+apollo.applyMiddleware({ app, cors: false });
 
 export default app;
